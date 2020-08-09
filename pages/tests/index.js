0977c6df4b40df9c870c8b3137b5495719c61e57
dcondo10 <dcondo@libelulasoft.com>
2020-08-08 23:03:00 -0500

    tests con el nuevo editor

---
+++ pages/tests/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PublicLayout from '@layouts/publicLayout';
+
+const TestsContainer = () => {
+  return (
+    <PublicLayout>
+      <main className="container-fluid">
+        <div className="row">
+          <div className="col-12"></div>
+        </div>
+      </main>
+    </PublicLayout>
+  );
+};
+
+export default TestsContainer;
