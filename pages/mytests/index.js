ec5d08ab7fa5c7d2c931cc1cb1ed49c77c6d0c1b
dcondo10 <dcondo@libelulasoft.com>
2020-08-08 23:15:15 -0500

    testings

--- pages/mytests/index.js
+++ pages/mytests/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
 import PublicLayout from '@layouts/publicLayout';
 
+import React, { useState } from 'react';
+
 const TestsContainer = () => {
+  const [content, setContent] = useState('');
+
   return (
     <PublicLayout>
       <main className="container-fluid">
         <div className="row">
-          <div className="col-12"></div>
+          <div className="col-12">
+            <h1>Test</h1>
+          </div>
         </div>
       </main>
     </PublicLayout>
