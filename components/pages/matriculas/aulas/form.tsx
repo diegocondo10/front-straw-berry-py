f01c3837712ff498ac0e552faaf537e76247a6f4
dcondo10 <dcondo@libelulasoft.com>
2020-10-01 22:37:55 -0500

    ADD: trabajando en el form de aula

--- components/pages/matriculas/aulas/form.tsx
+++ components/pages/matriculas/aulas/form.tsx
@@ -3,6 +3,7 @@ import FooterButtonsForm from '@components/Buttons/FooterButtonsForm';
 import CustomDropdown from '@components/forms/CustomDropdown';
 import CustomInputNumber from '@components/forms/CustomInputNumber';
 import CustomMultiSelect from '@components/forms/CustomMultiSelect';
+import CustomTextArea from '@components/forms/CustomTextArea';
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
@@ -33,7 +34,7 @@ const AulasFormContainer = ({
         <TitleBreadCrumb title={title} items={items} />
 
         <div className="row justify-content-center">
-          <div className="col-md-8 col-lg-6 jumbotron">
+          <div className="col-md-8 col-lg-7 jumbotron">
             <form onSubmit={methods.handleSubmit(localOnSubmit)}>
               <CustomDropdown
                 label="Seleccione el periodo lectivo:"
@@ -52,10 +53,58 @@ const AulasFormContainer = ({
                 optionLabel="persona.str"
                 filter
                 options={docentes}
-                rules={{ required: 'Este campo es obligatorio' }}
-              />
+                rules={{
+                  required: 'Este campo es obligatorio',
+                  validate: (docentesSeleccionados: any[]) => {
+                    if (docentesSeleccionados.length > 2) {
+                      return 'Solo puede escoger máximo 2 docentes por aula';
+                    }
 
-              <CustomInputNumber label="Capacidad:" name="capacidad" />
+                    return true;
+                  },
+                }}
+              />
+              <div className="form-row">
+                <div className="col-6">
+                  <CustomInputNumber
+                    label="Grado:"
+                    name="grado"
+                    prefix="grado: "
+                    rules={{
+                      required: 'Este campo es obligatorio',
+                      min: {
+                        value: 1,
+                        message: 'Es grado minimo es 1',
+                      },
+                      max: {
+                        value: 15,
+                        message: 'El grado maximo es de 15',
+                      },
+                    }}
+                  />
+                </div>
+
+                <div className="col-6">
+                  <CustomInputNumber
+                    label="Capacidad:"
+                    name="capacidad"
+                    suffix=" Alumnos"
+                    rules={{
+                      required: 'Este campo es obligatorio',
+                      min: {
+                        value: 2,
+                        message: 'La capacidad mínima es de 2 alumnos',
+                      },
+                      max: {
+                        value: 50,
+                        message: 'La capacidad máxima es de 50 alumnos',
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+
+              <CustomTextArea label="Observaciones" name="observaciones" />
 
               <FooterButtonsForm hrefBack="/matriculas/aulas" loading={loading} />
             </form>
