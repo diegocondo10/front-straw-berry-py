519838b0eb1f1883eaf4c14e6be3904b49e1d1f2
Yani92 <yanif13@outlook.com>
2020-10-15 23:54:22 -0500

    form matriculas

---
+++ components/pages/matriculas/form.tsx
@@ -0,0 +1,128 @@
+import BreadCrumbTitle from '@components/BreadCrumbs/titleBreadCrumb';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Form } from 'react-bootstrap';
+import CustomTextInput from '@components/forms/CustomTextInput';
+import CustomInputNumber from '@components/forms/CustomInputNumber';
+import CustomTextArea from '@components/forms/CustomTextArea';
+import FooterButtonsForm from '@components/Buttons/FooterButtonsForm';
+import CustomDropDown from '@components/forms/CustomDropDown';
+
+const MatriculaFormContainer = ({ title, items, onSubmit, aulas = [], alumnos = [], defaultData, }: {
+    title: string;
+    items: any[];
+    aulas: any[];
+    alumnos: any[];
+    onSubmit: CallableFunction;
+    defaultData?: any;
+}) => {
+    const methods = useForm({
+        mode: 'onChange',
+        defaultValues: defaultData,
+    });
+
+    const localOnSubmit = async (input) => {
+        input.aula = getId(input.aula);
+        input.alumnos = getId(input.alumnos);
+        await onSubmit(input);
+    };
+
+    const { handleSubmit } = methods;
+
+    return (
+        <main className="container-fluid">
+            <BreadCrumbTitle title={title} items={items} />
+            <FormProvider {...methods}>
+                <div className="row justify-content-center">
+                    <div className="col-md-10 jumbotron rounded">
+                        <form onSubmit={methods.handleSubmit(localOnSubmit)}>
+                            <Form.Row>
+                                <div className="col-md-12">
+                                    <CustomDropDown
+                                        label="Seleccione Aula:"
+                                        name="aula"
+                                        optionLabel="nombre"
+                                        filter
+                                        options={aulas}
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomDropDown
+                                        label="Seleccione Alumno:"
+                                        name="alumno"
+                                        optionLabel="nombre"
+                                        filter
+                                        options={alumnos}
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomTextInput
+                                        label="Tratamiento:"
+                                        name="tratamiento"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomTextArea
+                                        label="Diagnóstico:"
+                                        name="diagnostico"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomTextInput
+                                        label="Matrícula:"
+                                        name="matricula"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomInputNumber
+                                        label="Número de Matrícula:"
+                                        name="numeroMatricula"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomInputNumber
+                                        label="Aporte Voluntario:"
+                                        name="aporteVoluntario"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-md-12">
+                                    <CustomTextArea
+                                        label="Diagnóstico Final:"
+                                        name="diagnosticoFinal"
+                                        rules={{
+                                            required: 'Este campo es obligatorio',
+                                        }}
+                                    />
+                                </div>
+                            </Form.Row>
+                            <FooterButtonsForm loading={false} hrefBack="/matriculas" />
+                        </form>
+                    </div>
+                </div>
+            </FormProvider>
+        </main>
+    );
+};
+
+export default MatriculaFormContainer;
