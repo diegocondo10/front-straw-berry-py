d0fc73aec778a211c5c56b85adc087483c4d0752
MrRainx <diegocondo1007@gmail.com>
2021-03-01 03:17:14 -0500

    REFACTOR: url prod

--- urls.ts
+++ urls.ts
@@ -1,1 +1,4 @@
-export const URL_BASE = process.env.URL_BASE || 'http://localhost:9000/';
+// const urlDefault = 'http://localhost:9000/';
+const urlDefault = 'http://ipca.us-west-2.elasticbeanstalk.com/';
+
+export const URL_BASE = process.env.URL_BASE || urlDefault;
