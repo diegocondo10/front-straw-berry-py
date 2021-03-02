8dd64287ad81bc11881b6a7f5cff15ca0fb0e758
MrRainx <diegocondo1007@gmail.com>
2021-03-02 00:39:07 -0500

    REFACTOR: urls heroku

--- urls.ts
+++ urls.ts
@@ -1,4 +1,4 @@
 // const urlDefault = 'http://localhost:9000/';
-const urlDefault = 'http://ipca.us-west-2.elasticbeanstalk.com/';
+const urlDefault = 'https://straw-berry-py.herokuapp.com/';
 
 export const URL_BASE = process.env.URL_BASE || urlDefault;
